8fb84b642d885399e055b92cb3c85254fa5c5a59
Fernando dos Santos Batista <fernandodossantos005@gmail.com>
2022-04-07 10:26:12 -0300

    Update api.js

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import utilsStorage from '../utils/storage';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1'
+    baseURL: 'https://apipetbackend.herokuapp.com/api/v1'
 });
 
 api.interceptors.request.use((config) => {
